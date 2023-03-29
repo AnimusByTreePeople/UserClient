e2c63c5a817214f04e488a7b1a6cf954e3d20fe4
Danilka-Akarawita <danilka.20210514@iit.ac.lk>
2023-03-30 02:44:40 +0530

    feat:add asset.jsx

---
+++ ANIMUS/src/Pages/Asset.jsx
@@ -0,0 +1,61 @@
+import {
+    ChakraProvider,
+    Heading,
+    Container,
+    Text,
+    Input,
+    Button,
+    Wrap,
+    Stack, 
+    Image,
+    Link,
+    SkeletonCircle,
+    SkeletonText,
+  } from "@chakra-ui/react";
+  import axios from "axios";
+  import { useState } from "react";
+
+  import "./Asset.css";
+  
+  const App = () => {
+    const [image, updateImage] = useState();
+    const [prompt, updatePrompt] = useState();
+    const [loading, updateLoading] = useState();
+  
+    const generate = async (prompt) => {
+      updateLoading(true);
+      const result = await axios.get(`http://127.0.0.1:8000/?prompt=${prompt}`);
+      updateImage(result.data);
+      updateLoading(false);
+    };
+  
+    return (
+      <ChakraProvider>
+        <Container>
+          
+  
+          <Wrap margin={"100px"}>
+            <Input
+              value={prompt}
+              onChange={(e) => updatePrompt(e.target.value)}
+              width={"350px"}
+            ></Input>
+            <Button onClick={(e) => generate(prompt)} colorScheme={"yellow"}>
+              Generate
+            </Button>
+          </Wrap>
+  
+          {loading ? (
+            <Stack>
+              <SkeletonCircle />
+              <SkeletonText />
+            </Stack>
+          ) : image ? (
+            <Image src={`data:image/png;base64,${image}`} boxShadow="lg" />
+          ) : null}
+        </Container>
+      </ChakraProvider>
+    );
+  };
+  
+  export default App;
