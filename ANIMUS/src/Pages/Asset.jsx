2bbcb0d3e4973a2c3110b39e31e16b52f1a28ab9
Suraj Wijewickrama <90107939+SurajWijewickrama@users.noreply.github.com>
2023-04-04 19:17:36 +0530

    Assets update

--- ANIMUS/src/Pages/Asset.jsx
+++ ANIMUS/src/Pages/Asset.jsx
@@ -7,14 +7,16 @@ const App = () => {
   const [file, setFile] = useState();
   const [prompt, setPrompt] = useState("");
   const [loading, setLoading] = useState();
+  const [upload, setUpload] = useState(null);
+  const [baseModel, setBaseModel] = useState(null);
   const { account } = useAccountContext();
 
   const generate = async (prompt) => {
     console.log(prompt);
     setLoading(true);
-    const result = await fetch(`http://127.0.0.1:8000/?prompt=${prompt}`);
+    const resultString = prompt + " UV map for " + baseModel;
+    const result = await fetch(`http://127.0.0.1:8000/?prompt=${resultString}`);
     const data = await result.json();
-    setImage(result.data);
     console.log(data);
     setLoading(false);
   };
@@ -60,6 +62,77 @@ const App = () => {
       }
     }
   };
+
+  const uploadImage = () => {
+    console.log("ipload" + upload);
+    if (upload) {
+      return (
+        <div className="flex flex-col py-4">
+          <h1 className="m-1">
+            Upload your own image below for a "{`${baseModel}`}" Skin
+          </h1>
+          <input
+            type="file"
+            name="uploaded_file"
+            onChange={(e) => setFile(e.target.files)}
+            className="h-10 p-2 mb-4"
+          />
+          <button onClick={handleSubmitButton}>Submit</button>
+        </div>
+      );
+    } else {
+      return (
+        <div className="flex items-center">
+          <h1 className="m-1">Or you can upload your own image,</h1>
+          <button className="m-4" onClick={(e) => setUpload(true)}>
+            Upload Image
+          </button>
+        </div>
+      );
+    }
+  };
+
+  const selectBaseModel = () => {
+    console.log("here" + baseModel);
+    if (baseModel) {
+      return (
+        <div className="flex flex-col py-4">
+          <h1 className="font-lemon text-3xl text-center">
+            Enter any text of your imagination and Animus AI will generate the
+            skin for you
+          </h1>
+          <h1 className="m-1">
+            keep in mind you are creating a Skin for the {`${baseModel}`}{" "}
+          </h1>
+          <input
+            value={prompt}
+            placeholder="Enter the prompt "
+            onChange={(e) => setPrompt(e.target.value)}
+            className="h-10 p-2 mb-4"
+          ></input>
+          <button className="m-4" onClick={(e) => generate(prompt)}>
+            Generate
+          </button>
+        </div>
+      );
+    } else {
+      return (
+        <div className="flex flex-col items-center m-4">
+          <h1 className="font-lemon text-3xl text-center m-1">
+            Generate unique skins for your Stuff in Game!{" "}
+          </h1>
+          <h1>Select the base model</h1>
+          <button
+            className="m-4 h-20 w-20"
+            onClick={(e) => setBaseModel("Sphere")}
+          >
+            Sphere
+          </button>
+        </div>
+      );
+    }
+  };
+
   if (!account) {
     return (
       <div className="flex flex-col shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)] p-4 m-8 rounded-lg bg-gradient-to-r  from-green-800 to-teal-900 ">
@@ -76,26 +149,10 @@ const App = () => {
     );
   } else {
     return (
-      <div className="flex flex-col shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)] p-16 m-8 rounded-lg bg-gradient-to-r  from-green-800 to-teal-900 ">
-        <div className="flex flex-col py-4">
-          <input
-            value={prompt}
-            placeholder="Enter the prompt "
-            onChange={(e) => setPrompt(e.target.value)}
-            className="h-10 p-2 mb-4"
-          ></input>
-          <button onClick={(e) => generate(prompt)}>Generate</button>
-        </div>
-        <div className="flex flex-col py-4">
-          <input
-            type="file"
-            name="uploaded_file"
-            onChange={(e) => setFile(e.target.files)}
-            className="h-10 p-2 mb-4"
-          />
-          <button onClick={handleSubmitButton}>Submit</button>
-        </div>
-        <button className="h-10 p-2 mb-4" onClick={handleShowButton}>
+      <div className="flex flex-col shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)] p-16 m-8 items-center rounded-lg bg-gradient-to-r  from-green-800 to-teal-900 ">
+        {selectBaseModel()}
+        {uploadImage()}
+        <button className="h-10 p-2 m-4" onClick={handleShowButton}>
           Show Image
         </button>
 
